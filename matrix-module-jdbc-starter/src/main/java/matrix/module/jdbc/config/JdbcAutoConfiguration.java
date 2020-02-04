d50072413f272d9de1b6a9d255dcc505ed41c297
wangcheng <365092422@qq.com>
2020-02-04 20:27:15 +0800

    jdbc模块添加

--- matrix-module-jdbc-starter/src/main/java/matrix/module/jdbc/config/JdbcAutoConfiguration.java
+++ matrix-module-jdbc-starter/src/main/java/matrix/module/jdbc/config/JdbcAutoConfiguration.java
@@ -1,8 +1,28 @@
 package matrix.module.jdbc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
 /**
  * @author WangCheng
  * @date 2020/2/4
  */
+@Configuration
+@AutoConfigureAfter({DatabaseAutoConfiguration.class})
+@ConditionalOnProperty(value = {"jdbc.enabled"})
 public class JdbcAutoConfiguration {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource);
+    }
 }
