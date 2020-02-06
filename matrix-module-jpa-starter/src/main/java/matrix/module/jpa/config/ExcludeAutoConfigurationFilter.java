f032195361575c4d0f325b668df8137e1c2eed39
wangcheng <365092422@qq.com>
2020-02-06 12:10:55 +0800

    jpa模块添加

---
+++ matrix-module-jpa-starter/src/main/java/matrix/module/jpa/config/ExcludeAutoConfigurationFilter.java
@@ -0,0 +1,28 @@
+package matrix.module.jpa.config;
+
+import org.springframework.boot.autoconfigure.AutoConfigurationImportFilter;
+import org.springframework.boot.autoconfigure.AutoConfigurationMetadata;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author WangCheng
+ * @date 2020/2/5
+ */
+public class ExcludeAutoConfigurationFilter implements AutoConfigurationImportFilter {
+
+    private static final Set<String> SHOULD_SKIP = new HashSet<>(Arrays.asList(
+            "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration",
+            "org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration"));
+
+    @Override
+    public boolean[] match(String[] classNames, AutoConfigurationMetadata metadata) {
+        boolean[] matches = new boolean[classNames.length];
+        for (int i = 0; i < classNames.length; i++) {
+            matches[i] = !SHOULD_SKIP.contains(classNames[i]);
+        }
+        return matches;
+    }
+}
